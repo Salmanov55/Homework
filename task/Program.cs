e550a327ddf0da5bbfeb3a285d32d2a49c6392b0
Ali <lisalmanov84@gmail.com>
2022-08-03 18:24:20 +0400

    homework

---
+++ task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 4, 3, 1, 5, 6, }; int min = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+            }
+            Console.WriteLine(min);
+
+        }
+    }
+}
+
